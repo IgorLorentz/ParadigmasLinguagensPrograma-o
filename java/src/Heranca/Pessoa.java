9b4d041ff03edf49420dfc68eb6b0002e4022cad
mannelz <emannuelm29@gmail.com>
2024-06-09 19:13:36 -0300

    create heranca

---
+++ java/src/Heranca/Pessoa.java
@@ -0,0 +1,13 @@
+package Heranca;
+
+public class Pessoa 
+{
+    protected String nome;
+    protected String logradouro;
+    protected String bairro;
+    protected String cidade;
+    protected String estado;
+    protected int cep;
+    protected int telefone;
+    protected String email;
+}
