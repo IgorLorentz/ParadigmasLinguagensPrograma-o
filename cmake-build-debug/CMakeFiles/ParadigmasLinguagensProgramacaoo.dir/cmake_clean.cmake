212fa40e24cf001e4ba59e980c996b2d1b81e1e3
IgorLorentz <igorlorentz81@gmail.com>
2024-06-10 15:16:34 -0300

    cmake

--- cmake-build-debug/CMakeFiles/ParadigmasLinguagensProgramacaoo.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/ParadigmasLinguagensProgramacaoo.dir/cmake_clean.cmake
@@ -1,6 +1,8 @@
 file(REMOVE_RECURSE
   "CMakeFiles/ParadigmasLinguagensProgramacaoo.dir/cpp/HelloWorld.cpp.o"
   "CMakeFiles/ParadigmasLinguagensProgramacaoo.dir/cpp/HelloWorld.cpp.o.d"
+  "CMakeFiles/ParadigmasLinguagensProgramacaoo.dir/cpp/Heranca/PessoaJuridica.cpp.o"
+  "CMakeFiles/ParadigmasLinguagensProgramacaoo.dir/cpp/Heranca/PessoaJuridica.cpp.o.d"
   "CMakeFiles/ParadigmasLinguagensProgramacaoo.dir/cpp/Ponteiros.cpp.o"
   "CMakeFiles/ParadigmasLinguagensProgramacaoo.dir/cpp/Ponteiros.cpp.o.d"
   "ParadigmasLinguagensProgramacaoo"
