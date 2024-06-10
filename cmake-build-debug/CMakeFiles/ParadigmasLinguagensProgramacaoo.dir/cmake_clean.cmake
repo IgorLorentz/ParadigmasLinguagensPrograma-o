77235da945ccb1a7e1ad3ab84adf9f5811163844
IgorLorentz <igorlorentz81@gmail.com>
2024-06-10 13:48:56 -0300

    Cmake

---
+++ cmake-build-debug/CMakeFiles/ParadigmasLinguagensProgramacaoo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ParadigmasLinguagensProgramacaoo.dir/cpp/HelloWorld.cpp.o"
+  "CMakeFiles/ParadigmasLinguagensProgramacaoo.dir/cpp/HelloWorld.cpp.o.d"
+  "ParadigmasLinguagensProgramacaoo"
+  "ParadigmasLinguagensProgramacaoo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ParadigmasLinguagensProgramacaoo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
