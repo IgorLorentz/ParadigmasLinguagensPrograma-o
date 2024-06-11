cc1cb17614bfd9d7191204eabb70ddd6473d8386
IgorLorentz <igorlorentz81@gmail.com>
2024-06-11 18:42:47 -0300

    Update Polimorfismo

--- java/src/Polimorfismo/Figuras_Main.java
+++ java/src/Polimorfismo/Figuras_Main.java
@@ -1,5 +1,7 @@
 package Polimorfismo;
+
 import java.util.Scanner;
+
 public class Figuras_Main {
     public static void main(String[] args) {
         int opcao = -1;
@@ -7,8 +9,7 @@ public class Figuras_Main {
         String nome;
 
         Scanner scanner = new Scanner(System.in);
-        while (opcao != 0)
-        {
+        while (opcao != 0) {
             System.out.println
                     (
                             "Selecione a opção desejada:"
@@ -21,17 +22,22 @@ public class Figuras_Main {
 
             opcao = scanner.nextInt();
 
-            switch (opcao)
-            {
+            switch (opcao) {
                 case 1:
                     nome = "Esfera";
                     System.out.println("Informe o raio!");
                     dim1 = scanner.nextDouble();
-                    Esfera esfera = new Esfera( nome, dim1);
+                    Esfera esfera = new Esfera(nome, dim1);
+
+                    esfera.area();
+                    esfera.volume();
 
                     esfera.display();
                     esfera.aumentar();
                     esfera.display();
+
+                    esfera.area();
+                    esfera.volume();
                     break;
 
                 case 2:
@@ -40,11 +46,17 @@ public class Figuras_Main {
                     dim1 = scanner.nextDouble();
                     System.out.println("Informe a altura!");
                     dim2 = scanner.nextDouble();
-                    Cilindro cilindro = new Cilindro( nome, dim1, dim2);
+                    Cilindro cilindro = new Cilindro(nome, dim1, dim2);
+
+                    cilindro.area();
+                    cilindro.volume();
 
                     cilindro.display();
                     cilindro.aumentar();
                     cilindro.display();
+
+                    cilindro.area();
+                    cilindro.volume();
 
                     break;
 
@@ -56,11 +68,18 @@ public class Figuras_Main {
                     dim2 = scanner.nextDouble();
                     System.out.println("Informe a altura!");
                     dim3 = scanner.nextDouble();
-                    Cone cone = new Cone( nome, dim1, dim2, dim3);
+                    Cone cone = new Cone(nome, dim1, dim2, dim3);
+
+                    cone.area();
+                    cone.volume();
 
                     cone.display();
                     cone.aumentar();
                     cone.display();
+
+                    cone.area();
+                    cone.volume();
+
                     break;
 
                 case 4:
@@ -71,11 +90,18 @@ public class Figuras_Main {
                     dim2 = scanner.nextDouble();
                     System.out.println("Informe a profundidade!");
                     dim3 = scanner.nextDouble();
-                    Paralelepipedo paralelepipedo = new Paralelepipedo( nome, dim1, dim2, dim3);
+                    Paralelepipedo paralelepipedo = new Paralelepipedo(nome, dim1, dim2, dim3);
+
+                    paralelepipedo.area();
+                    paralelepipedo.volume();
 
                     paralelepipedo.display();
                     paralelepipedo.aumentar();
                     paralelepipedo.display();
+
+                    paralelepipedo.area();
+                    paralelepipedo.volume();
+                    
                     break;
             }
         }
